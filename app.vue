320766b78ca8c2289c22953737739b2b1428eac2
Mads Welling <54644486+MadsWAndersen@users.noreply.github.com>
2023-10-09 12:37:57 +0200

    Add default folders + default setup

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1 class="text-3xl font-bold">
-    Umbraco Bachelor
-  </h1>
+  <NuxtLayout>
+    <NuxtPage></NuxtPage>
+  </NuxtLayout>
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h1>Umbraco</h1>
+    </div>
+</template>
