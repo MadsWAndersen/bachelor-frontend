bd051b8747c6622e0752ad57344dd2326e20e6bb
Mads Welling <54644486+MadsWAndersen@users.noreply.github.com>
2023-11-16 10:50:27 +0100

    Add Github Action

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,9 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
-
+  nitro: {  
+    preset: 'netlify'  
+  },  
   postcss: {
     plugins: {
       tailwindcss: {
