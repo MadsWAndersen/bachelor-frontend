8f8c81cb1100eba90212ec635b4824f07efa993d
Jacob Welander Jensen <64834767+Welander1994@users.noreply.github.com>
2023-11-20 12:01:25 +0100

    adds tailwind stylling

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -40,36 +40,40 @@ let login = () => {
 </script>
 
 <template>
-    <div>
-        <div class="w-full max-w-xs">
-            <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-                <div class="mb-4">
-                    <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
-                        Username
-                    </label>
-                    <input v-model="username"
-                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                        id="username" type="text" placeholder="Username">
+    <div class="container">
+        <div class="container-row">
+            <div class="col-span-full">
+                <div class="w-full max-w-xs">
+                    <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                        <div class="mb-4">
+                            <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+                                Username
+                            </label>
+                            <input @keyup.enter="login" v-model="username"
+                                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                id="username" type="text" placeholder="Username">
+                        </div>
+                        <div class="mb-6">
+                            <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+                                Password
+                            </label>
+                            <input @keyup.enter="login" v-model="password"
+                                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                                id="password" type="password" placeholder="******************">
+                            <p v-if="!fetchResult.access_token" class="text-red-500 text-xs italic">{{
+                                !fetchResult.error_description ?
+                                fetchResult.error : fetchResult.error_description }}</p>
+                        </div>
+                        <div class="flex items-center justify-between">
+                            <button @click="login"
+                                class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                                type="button">
+                                Sign In
+                            </button>
+                        </div>
+                    </form>
                 </div>
-                <div class="mb-6">
-                    <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
-                        Password
-                    </label>
-                    <input v-model="password"
-                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-                        id="password" type="password" placeholder="******************">
-                    <p v-if="!fetchResult.access_token" class="text-red-500 text-xs italic">{{
-                        !fetchResult.error_description ?
-                        fetchResult.error : fetchResult.error_description }}</p>
-                </div>
-                <div class="flex items-center justify-between">
-                    <button @click="login"
-                        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                        type="button">
-                        Sign In
-                    </button>
-                </div>
-            </form>
+            </div>
         </div>
         <p v-if="fetchResult.access_token">{{ fetchResult.access_token }}</p>
 
