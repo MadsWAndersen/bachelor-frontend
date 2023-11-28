1b33c7eb05048066dbf0f4d77a5b73282ad42826
Mads Welling <54644486+MadsWAndersen@users.noreply.github.com>
2023-11-28 08:34:56 +0100

    Add components page

---
+++ pages/components/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="container">
+    <div class="container-row">
+      <div class="col-span-full">
+        <h1>Insert components here</h1>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
