9163ff46314b8f7e07d7f677b94b09ed3ba104f7
Mads Welling <54644486+MadsWAndersen@users.noreply.github.com>
2023-11-29 10:34:20 +0100

    Google fonts on

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "nuxt-icon", "@pinia/nuxt"],
+  modules: ["@nuxtjs/tailwindcss", "nuxt-icon", "@pinia/nuxt", '@nuxtjs/google-fonts'],
   css: ['~/assets/css/main.css'],
   nitro: {
     preset: 'netlify'
@@ -17,22 +17,11 @@ export default defineNuxtConfig({
   app: {
     pageTransition: { name: 'slide', mode: 'out-in' }
   },
-/*   runtimeConfig: {
-    // The private keys which are only available within server-side
-    UmbProjectAlias: 'pba-webdev',
-    AcceptLanguage: 'en-US',
-    Authorization: 'Basic elFJZk50eEpCYWFidFFDSTNweDg6',
-    public: {
-      apiBase: '/api',
-    }
-
-  }, */
-
-/*   googleFonts: {
+ googleFonts: {
     families: {
       Lato: [100, 200, 300, 400, 500, 600, 700],
     }
-  } */
+  }
 
 
 
