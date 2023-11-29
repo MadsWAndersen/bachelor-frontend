ff27e1f902c51b72c8ab3525fd7481611b4b3530
idax6797 <idax6797@edu.ucl.dk>
2023-11-29 21:29:28 +0100

    Revert "init format"
    This reverts commit 359b7a16f490ab2497992ec2ca1e926092c1e7a3.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,11 +13,6 @@ export default defineNuxtConfig({
       autoprefixer: {},
     },
   },
-  prettier: {
-    tabWidth: 2,
-    singleQuote: true,
-    semi: false
-  },
 
   app: {
     pageTransition: { name: 'slide', mode: 'out-in' }
