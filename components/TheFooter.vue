9ae1e9570dc5987f6832e63db6b384a392f4a605
Mads Welling <54644486+MadsWAndersen@users.noreply.github.com>
2023-11-29 10:18:21 +0100

    Change node v to 16

---
+++ components/TheFooter.vue
@@ -0,0 +1,18 @@
+<template>
+  <footer class="container p-4 mt-10">
+    <div class="container-row p-4">
+      <div class="col-span-full">
+        <p class="text-white">Umbraco</p>
+        <div class="space-x-4 text-white">
+          <NuxtLink to="/">Home</NuxtLink>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<style scoped>
+.umbraco-footer {
+  background-color: #35404d;
+}
+</style>
---
+++ components/profilePicture.vue
@@ -0,0 +1,11 @@
+<template>
+  <img
+    :src="image"
+    alt="test"
+    class="rounded-full h-[40px] w-[40px] object-cover"
+  />
+</template>
+
+<script setup>
+const props = defineProps(["image"]);
+</script>
