7daa6485ac0c6dbb7e9ec26397f811488b453dfb
Jacob Welander Jensen <64834767+Welander1994@users.noreply.github.com>
2023-12-04 21:08:25 +0100

    adds slack notifications througt pipedream

--- pages/submission/index.vue
+++ pages/submission/index.vue
@@ -62,6 +62,8 @@ let submit = async () => {
         }
     });
     console.log(raw);
+
+    // ping the editors on slack 
     var requestOptions = {
         method: 'POST',
         headers: myHeaders,
@@ -73,6 +75,26 @@ let submit = async () => {
         'https://api.umbraco.io/content',
         requestOptions,
     )
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify({
+        "text": "event"
+    });
+
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+    };
+
+    fetch("https://eocet9yn9ivqaq7.m.pipedream.net", requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
+
+    // redirects user
     await navigateTo('/documentation')
 }
 </script>
