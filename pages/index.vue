6af66a5a9f7345fd146f99c90349ad31cd2d96b9
Mads Welling <54644486+MadsWAndersen@users.noreply.github.com>
2023-12-06 12:52:35 +0100

    Lint fixes

--- pages/index.vue
+++ pages/index.vue
@@ -15,13 +15,23 @@
 
 		<div v-if="cmsContent">
 			<div v-if="cmsContent.childrenData._embedded.content" class="-mb-3">
-				<div v-for="(content, index) in cmsContent.childrenData._embedded
-					.content" :key="index">
-					<div v-for="(childContent, index) in content.childrenData
-						._embedded.content" :key="index" class="mb-3">
+				<div
+					v-for="(content, index) in cmsContent.childrenData._embedded
+						.content"
+					:key="index">
+					<div
+						v-for="(childContent, index) in content.childrenData
+							._embedded.content"
+						:key="index"
+						class="mb-3">
 						<div v-if="childContent.highligted">
-							<HighlightCard :highlightedColor="'bg-um-red'" :title="childContent.name"
-								:bodyText="childContent.documentationDescription" :url="childContent._url" />
+							<HighlightCard
+								:highlightedColor="'bg-um-red'"
+								:title="childContent.name"
+								:bodyText="
+									childContent.documentationDescription
+								"
+								:url="childContent._url" />
 						</div>
 					</div>
 				</div>
@@ -29,8 +39,15 @@
 		</div>
 
 		<div class="container-row my-auto">
-			<sectionCard v-for="(card, index) in data._embedded.content" :key="index" :url="card._url" :headline="card.name"
-				:description="card.description" :image="card.image" :icon="card.icon?.src" :style="'dark'">
+			<sectionCard
+				v-for="(card, index) in data._embedded.content"
+				:key="index"
+				:url="card._url"
+				:headline="card.name"
+				:description="card.description"
+				:image="card.image"
+				:icon="card.icon?.src"
+				:style="'dark'">
 			</sectionCard>
 		</div>
 	</div>
